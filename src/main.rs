576b1a6b0bdee2fe34c33b90bf9aef50be33f8ab
Ermir Alliu <mirialliu@gmail.com>
2025-11-29 00:52:58 +0100

    Somewhat working, needs more logic, but at least I fixed an error that had to do with base_form not being reinitialized

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,30 @@ use std::vec;
 
 use unicode_normalization::UnicodeNormalization;
 
-const VOWELS: [char; 7] = ['a', 'e', 'ë', 'i', 'o', 'u', 'y'];
-// the letter ë has two ways of being expressed that we should account for:
-const NFC_E_DIAERESIS_2B: [u8; 2] = [0xC3, 0xAB];
-const NFD_E_DIAERESIS_3B: [u8; 3] = [0x65, 0xCC, 0x88];
+// const VOWELS: [char; 7] = ['a', 'e', 'ë', 'i', 'o', 'u', 'y'];
+// // the letter ë has two ways of being expressed that we should account for:
+// const NFC_E_DIAERESIS_2B: [u8; 2] = [0xC3, 0xAB];
+// const NFD_E_DIAERESIS_3B: [u8; 3] = [0x65, 0xCC, 0x88];
 // When converted to &str: "ë"
 
-fn suffix_to_possibilities(suffix: &str) -> Option<Vec<&str>> {
-    // "" means the suffix is removed,
-    // None means try sth else
-    let mat = match suffix {
-        // Even different arms that return the same thing, the Rust optmizer treats them as the same thing
-        // This way we can keep different logic with the same result in different arms for clarity
-        //zgj1. kr.thj + pjesore
-        "ova" | "ove" | "oi" | "uam" | "uat" | "uan" | "uar" => vec!["oj", "uaj"],
-        "jta" | "jte" | "jti" | "jtëm" | "jtët" | "jtën" | "jtur" => vec!["j"], // mbajta and
-        // friends, easy
-        // match but waste
-        // of performance,
-        // maybe?
-        "va" | "ve" | "u" | "më" | "të" | "në" => vec!["j", "e", ""], // + "e" per shtie? but really low
-        // probability on that ngl.
-        // The empty option per "pi"
-        // Let's be as inclusive as possible
-        // and discuss probabilities later
-        "rë" => vec!["j"], // larë and bërë -> are different in some forms (lava, bëra) but the same in this
-        "ja" | "je" | "nim" | "nit" | "nin" => vec!["j", "", "e"], // + "e" per shtie?
-        // edhe "" por ndryshon zanorja
-        // e fundit nga i ne ë (vija ->
-        // vë)
-        "nte" => vec!["j"],
-        "j" | "n" | "jmë" | "jne" => vec!["j"],
-        "ni" => vec!["j"], // shkruani, vendosni => different in some forms but the same here
-        "ta" | "ti" | "tëm" | "tët" | "tën" | "tur" => vec!["j", ""], // "" per di -> dita
-        // and friends
-        "te" => vec!["j", ""], // arrite, vendoste => different groupings
-        "im" | "in" => vec![""],
-        "ra" | "ri" => vec!["j"],
-        "a" | "e" | "i" | "ëm" | "ët" | "ën" | "ë" | "ur" => vec![""], // This needs special
-        // treatment however,
-        // because of words like
-        // dal dhe heq that change
-        // their vowel in past
-        // simple
-        _ => return None, // Now that I think about it, if it matches none of these, then it just
-                          // might be simply one that is okay the way it is ""
-                          // We will need to match in the very beginning if it is the base form
-                          // though. So this is fine
-    };
-    Some(mat)
-}
-
-fn suffix_1char(ch: char) -> Option<Vec<&'static str>> {
+// fn suffix_1char(ch: char) -> Option<Vec<&'static str>> {
+//     // For now, I'm keeping it simple with
+//     // static lifetimes
+//     let mat = match ch {
+//         'u' => vec!["j", "e", ""],
+//         'j' | 'n' => vec!["j"],
+//         'a' | 'e' | 'i' | 'ë' => vec![""],
+//         _ => return None,
+//     };
+//     Some(mat)
+// }
+fn suffix_1char(ch: &str) -> Option<Vec<&'static str>> {
     // For now, I'm keeping it simple with
     // static lifetimes
     let mat = match ch {
-        'u' => vec!["j", "e", ""],
-        'j' | 'n' => vec!["j"],
-        'a' | 'e' | 'i' | 'ë' => vec![""],
+        "u" => vec!["j", "e", ""],
+        "j" | "n" => vec!["j"],
+        "a" | "e" | "i" | "ë" => vec![""],
         _ => return None,
     };
     Some(mat)
@@ -106,38 +72,70 @@ fn suffix_4char(st: &str) -> Option<Vec<&'static str>> {
 
 fn main() {
     println!("Hello, world!");
-    let verbs = vec!["punojmë", "punoni"];
+    let verbs = vec!["punojmë", "punuam", "shkruar", "lexuar", "vendosur"];
     verb_to_base(&verbs);
-    let Some(_) = suffix_to_possibilities("morea") else { return };
-    println!("This is never reached");
     // let Some(n) = return_some_option() else { return };
 }
 
-fn verb_to_base(verbs: &[&str]) {
-    // A slice of slices (kind of like an array of arrays)
+fn verb_to_base(verbs: &[&str]) { // A slice of slices (kind of like an array of arrays)
     let mut normalization_buffer = String::with_capacity(256);
-    let mut to_lowercase_buffer = String::with_capacity(256);
+    // Called main buffer because it's what's left after normalization and lowercasing
+    let mut main_buffer = String::with_capacity(256); 
+    let mut base_form = String::with_capacity(256);
     for &verb in verbs {
         // making sure the buffers are empty
         normalization_buffer.clear();
-        to_lowercase_buffer.clear();
+        main_buffer.clear();
+        base_form.clear();
         // adding the characters
+        // the function below normalizes three byte ë and ç to the 2 byte version.
         normalization_buffer.extend(verb.nfc());
-        // I don't like it but I feel like I was forced to use two different buffers.
-        to_lowercase_buffer.extend( // Had to do this because lowercasindsag created a String
-                                    // otherwise.
-                                    // I still have to read the assembly
-                                    // If the assembly is bad, I'll implement it myself
-            normalization_buffer
-                .chars()
-                .flat_map(|ch| ch.to_lowercase()),
-        ); 
-        // 4 characters
-        // 3 characters
+        // I don't like it but I was basically forced to use two different buffers.
+        main_buffer.extend(
+            // Had to do this because lowercasing would create a String otherwise.
+            // I still have to read the assembly. If the assembly is bad, I'll implement it myself
+            normalization_buffer.chars().flat_map(|ch| ch.to_lowercase()),
+        );
+        base_form.push_str(&main_buffer); // This is the variable we will test
+        // having the suffix functions be Option is looking like it was a terrible idea as of now
+        // I'm keeping this simple for now, usually the logic to get the next slice would be a
+        // little bit more advanced. This way I don't have to check the lengths manually though
+        // starting with 4 characters
+        possibilities_for_verb(4, &main_buffer, &mut base_form, suffix_4char);
+        base_form.clear();
+        base_form.push_str(&main_buffer); // This part will also need to be improved.
+        possibilities_for_verb(3, &main_buffer, &mut base_form, suffix_3char);
+        base_form.clear();
+        base_form.push_str(&main_buffer);
+        possibilities_for_verb(2, &main_buffer, &mut base_form, suffix_2char);
+        base_form.clear();
+        base_form.push_str(&main_buffer);
+        possibilities_for_verb(1, &main_buffer, &mut base_form, suffix_1char);
         // 2 characters
     }
 }
 
+type SuffixFunction = fn(&str) -> Option<Vec<&str>>;
+
+fn possibilities_for_verb(
+    suffix_char_len: usize,
+    main_buffer: &str, // This at the very least needs a better name
+    base_form: &mut String,
+    suffix_function: SuffixFunction,
+) {
+    // usually you know how much I don't like unnecessary functions but this is repeated 4 times,
+    // and this way it probably has more instruction cache advantages
+    if let Some((suffix_offset, _ch)) = main_buffer.char_indices().nth_back(suffix_char_len - 1) {
+        let suffix = &main_buffer[suffix_offset..];
+        println!("Verb: {}, Suffix: {} => ", main_buffer, suffix);
+        if let Some(possibilities) = suffix_function(suffix) {
+            for el in possibilities {
+                base_form.replace_range(suffix_offset.., el);
+                println!("{}", base_form);
+            }
+        }
+    }
+}
 // fn return_some_option() -> Option<i32> {
 //     Option::Some(234903)
 // }
