edbf06d5b189b88f42e24f9721db16b5f0764644
Ermir Alliu <mirialliu@gmail.com>
2025-11-30 01:49:27 +0100

    Adding the delimiters to start reading from the actual training/test data set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::vec;
+use std::{collections::HashMap, vec};
 
 use unicode_normalization::UnicodeNormalization;
 
@@ -70,17 +70,34 @@ fn suffix_4char(st: &str) -> Option<Vec<&'static str>> {
     Some(mat)
 }
 
+const WORD_DELIMITERS: &[u8] = &[b'.', b',', b'/', b'\\', b' ', b'\n', b'\t', b'\"', b'\'', b':', b';', b'!', b'?', b'(', b')', b'[', b']', b'-', b'\r'];
+const CONTENT_DELIMITER: &str = "/endarticle";
+const CATEGORY_DELIMITER: u8 = b'\n';
+// using &[u8] let's you have fun without worrying about sizes. Nice Rust stuff.
+// em dash might also appear, but I think that shit will likely have spaces around, so who cares.
+
 fn main() {
     println!("Hello, world!");
     let verbs = vec!["punojmë", "punuam", "shkruar", "lexuar", "vendosur"];
-    verb_to_base(&verbs);
+    let vocab_vector = vec!["punoj", "shkruaj", "lexoj", "vendos"]; // this is just for tests,
+    let mut map = HashMap::new();
+    for (i, &word) in vocab_vector.iter().enumerate() {
+        map.insert(word, i as u16);
+    }
+    let verb_ids = verb_to_base(&verbs, &map);
+    for el in verb_ids {
+        // placeholder function, normally will be the algorithms and whatnot
+        println!("{}", vocab_vector[el as usize]);
+    }
     // let Some(n) = return_some_option() else { return };
 }
 
-fn verb_to_base(verbs: &[&str]) { // A slice of slices (kind of like an array of arrays)
+fn verb_to_base(verbs: &[&str], map: &HashMap<&str, u16>) -> Vec<u16> {
+    // A slice of slices (kind of like an array of arrays)
+    let mut ids = Vec::new(); // not providing u16 directly to try keep it more open
     let mut normalization_buffer = String::with_capacity(256);
     // Called main buffer because it's what's left after normalization and lowercasing
-    let mut main_buffer = String::with_capacity(256); 
+    let mut main_buffer = String::with_capacity(256);
     let mut base_form = String::with_capacity(256);
     for &verb in verbs {
         // making sure the buffers are empty
@@ -101,18 +118,30 @@ fn verb_to_base(verbs: &[&str]) { // A slice of slices (kind of like an array of
         // I'm keeping this simple for now, usually the logic to get the next slice would be a
         // little bit more advanced. This way I don't have to check the lengths manually though
         // starting with 4 characters
-        possibilities_for_verb(4, &main_buffer, &mut base_form, suffix_4char);
+        if let Some(id) = possibilities_for_verb(4, &main_buffer, &mut base_form, suffix_4char, &map) {
+            ids.push(id);
+            continue;
+        }
         base_form.clear();
         base_form.push_str(&main_buffer); // This part will also need to be improved.
-        possibilities_for_verb(3, &main_buffer, &mut base_form, suffix_3char);
+        if let Some(id) = possibilities_for_verb(3, &main_buffer, &mut base_form, suffix_3char, &map) {
+            ids.push(id);
+            continue;
+        }
         base_form.clear();
         base_form.push_str(&main_buffer);
-        possibilities_for_verb(2, &main_buffer, &mut base_form, suffix_2char);
+        if let Some(id) = possibilities_for_verb(2, &main_buffer, &mut base_form, suffix_2char, &map) {
+            ids.push(id);
+            continue;
+        }
         base_form.clear();
         base_form.push_str(&main_buffer);
-        possibilities_for_verb(1, &main_buffer, &mut base_form, suffix_1char);
-        // 2 characters
+
+        if let Some(id) = possibilities_for_verb(1, &main_buffer, &mut base_form, suffix_1char, &map) {
+            ids.push(id);
+        }
     }
+    ids
 }
 
 type SuffixFunction = fn(&str) -> Option<Vec<&str>>;
@@ -122,19 +151,34 @@ fn possibilities_for_verb(
     main_buffer: &str, // This at the very least needs a better name
     base_form: &mut String,
     suffix_function: SuffixFunction,
-) {
+    map: &HashMap<&str, u16>, // Refactor later
+) -> Option<u16> {
     // usually you know how much I don't like unnecessary functions but this is repeated 4 times,
     // and this way it probably has more instruction cache advantages
     if let Some((suffix_offset, _ch)) = main_buffer.char_indices().nth_back(suffix_char_len - 1) {
         let suffix = &main_buffer[suffix_offset..];
-        println!("Verb: {}, Suffix: {} => ", main_buffer, suffix);
+        #[cfg(debug_assertions)]
+        {
+            println!("Verb: {}, Suffix: {}", main_buffer, suffix);
+        }
         if let Some(possibilities) = suffix_function(suffix) {
             for el in possibilities {
                 base_form.replace_range(suffix_offset.., el);
-                println!("{}", base_form);
+                // println!("{}", base_form);
+                if let Some(matching_word) = map.get(&base_form[..]) {
+                    // simple way of verifying a value and removing it in
+                    // production code.
+                    #[cfg(debug_assertions)]
+                    {
+                        dbg!(base_form);
+                    }
+                    return Some(*matching_word); // we return on the first match. This is a bit
+                    // this is a bit tightly coupled ngl
+                };
             }
         }
     }
+    None
 }
 // fn return_some_option() -> Option<i32> {
 //     Option::Some(234903)
