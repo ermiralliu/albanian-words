b3116db09e936fc117ebe2600f2c13b2027b2c10
Ermir Alliu <mirialliu@gmail.com>
2026-01-26 23:53:08 +0100

    reduced the vector preallocator by just a tiny bit

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         }
                     }
 
-                    let mut article_tokens = Vec::with_capacity(512);
+                    let mut article_tokens = Vec::with_capacity(256);
                     process_streaming(&mut local_buf, &mut|word| {
                         if /* stop_words.contains(word) || */ word[0].is_ascii_digit() {
                             return;
